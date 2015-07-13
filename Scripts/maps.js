a78730c1bd7c710d00d65f85a3f3ee27bf810177
dchen71 <dchen12812@gmail.com>
2015-07-13 14:26:08 -0700

    added basic google maps with lat long on las vegas

--- Scripts/maps.js
+++ Scripts/maps.js
@@ -1,8 +1,10 @@
-//Main url to get
-var getURL = function(){
-	return 'http://www.omdbapi.com/?'
+//Initializes the google maps on id googleMap
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(36.114647,-115.172813),
+    zoom:10,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
-
-var title = function(value){
-	return 't=' + value
-}
+google.maps.event.addDomListener(window, 'load', initialize);
