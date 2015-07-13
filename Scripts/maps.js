7a6dd628a69a2fb28d284752b31e628d9c2b598c
dchen71 <dchen12812@gmail.com>
2015-07-13 14:37:57 -0700

    added marker on vegas latlang

--- Scripts/maps.js
+++ Scripts/maps.js
@@ -1,10 +1,15 @@
 //Initializes the google maps on id googleMap
 function initialize() {
+  var vegas = new google.maps.LatLng(36.114647, -115.172813);
   var mapProp = {
-    center:new google.maps.LatLng(36.114647,-115.172813),
+    center:vegas,
     zoom:10,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var marker=new google.maps.Marker({
+  	position: vegas,
+  });
+  marker.setMap(map);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
